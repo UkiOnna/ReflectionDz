0520f5f136e2bcf3d9095c1b5043419b63d248e8
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-27 17:19:56 +0600

    wow

---
+++ DzReflection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            string password = "qwerty";
+            string login = "endless2008";
+
+            Console.WriteLine("Задание 2:");
+
+            user.Login = login;
+            user.Password = password;
+
+            Type type = typeof(User);
+
+            var properties = type.GetProperties();
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var name = properties[i];
+                Console.WriteLine(properties[i] + "  " + properties[i].GetValue(user));
+            }
+
+            Console.ReadLine();
+
+            Task1();
+            Console.ReadLine();
+        }
+
+        private static void Task1()
+        {
+            Console.WriteLine("Задание 1: ");
+
+            Type type = typeof(Console);
+
+            var membersInfo = type.GetMethods();
+
+            foreach (var info in membersInfo)
+            {
+                Console.WriteLine(info);
+            }
+        }
+    }
+}
